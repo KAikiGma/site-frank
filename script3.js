862416cbbf2aa5291aa5b529b6f306862ea8bf4e
KAikiGma <91917989+KAikiGma@users.noreply.github.com>
2022-09-05 21:00:33 -0300

    aula

--- script3.js
+++ script3.js
@@ -1,3 +1,15 @@
 function verificar() {
-    
+    var n1 = Number(document.getElementById('numero1').value);
+    var n2 = Number(document.getElementById('numero2').value);
+    var n3 = Number(document.getElementById('numero3').value);
+
+    if (n1 > n2 && n1 > n3) {
+        document.querySelector('p').innerHTML = `O maior número é ${n1}`;
+    } 
+    if (n2 > n1 && n2 > n3) {
+        document.querySelector('p').innerHTML = `O maior número é ${n2}`;
+    }  
+    if (n3 > n1 && n3 > n2) {
+        document.querySelector('p').innerHTML = `O maior número é ${n3}`;
+    }
 }
